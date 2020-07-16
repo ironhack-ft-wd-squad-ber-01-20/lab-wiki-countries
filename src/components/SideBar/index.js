ee108e252bf5cd01c54d142f78eb6f2c0409dcbf
carlosziegler <carlos.ziegler@gmail.com>
2020-07-16 17:44:15 +0200

    add flags

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -7,9 +7,13 @@ export default function index({ countries }) {
     return (
         <>
             <div className="sidebar">
-                <h3 className="w3-bar-item">Menu</h3>
                 {countries && countries.map((country) => {
-                    return (<Link key={country.id} to={`/country/${country?.cca3}`}>{country?.name?.common}</Link>)
+                    return (<Link className="country-link-container" key={country.id} to={`/country/${country?.cca3}`}>
+                        <img src={`https://www.countryflags.io/${country?.cca2}/flat/64.png`} alt="" />
+                        <span>
+                            {country?.name?.common}
+                        </span>
+                    </Link>)
                 })}
             </div>
         </>
